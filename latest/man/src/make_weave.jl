863c51aa32b48186bf9597959d0903e82ce216c1
autodocs <autodocs>
2017-03-16 00:42:04 +0000

    build based on 3d63a8b

---
+++ latest/man/src/make_weave.jl
@@ -0,0 +1,13 @@
+using Weave, Fontconfig, Cairo
+
+set_chunk_defaults(Dict{Symbol, Any}(:results => "hidden", :eval => false))
+
+weave(Pkg.dir("PhyloNetworks","docs","src", "man", "src","trait_tree.jmd"),
+      informat="markdown",
+      out_path = Pkg.dir("PhyloNetworks","docs","src", "man"),
+      fig_path = "../assets/figures",
+      fig_ext = ".pdf",
+      doctype = "github")
+
+# weave("docs/src/man/src/trait_tree.jmd", informat="markdown",
+    #   out_path = "docs/src/man/", doctype = "github")
