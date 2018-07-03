4812bef11b13bc3532c8818fed19502d31094291
Paul Bastide <paul.bastide@gmail.com>
2018-07-03 08:22:03 +0200

    Test branch for deployment.

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,8 @@ makedocs()
 
 deploydocs(
     deps   = Deps.pip("pygments", "mkdocs", "mkdocs-material", "python-markdown-math"),
-    repo = "github.com/crsl4/PhyloNetworks.jl.git",
+    repo = "github.com/pbastide/PhyloNetworks.jl.git",
     julia  = "0.6",
-    osname = "linux"
+    osname = "linux",
+    latest = "weave_doc"
 )
